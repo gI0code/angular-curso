14bd5a71401064e11f569d0c957f608cb84a0782
gI0 <gha_15@hotmail.com>
2021-11-24 08:23:31 -0600

    Se agrega manejo de error al no encontrar un pais

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,16 +10,20 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  error: boolean = false;
 
   constructor( private paisService: PaisService) {}
 
   buscar() {
+    this.error = false;
     console.log(this.termino);
 
     this.paisService.buscarPais( this.termino)
       .subscribe( resp => {
         console.log(resp)
-      })
+      }, (err) => {
+        this.error = true;
+      });
 
   }
  
